d00d233aee156d4fb038ed8684ce692521972a8b
cardavid7 <cardavid7@gmail.com>
2026-03-21 00:03:57 -0500

    update imports in importController

--- ms-item-core/src/main/java/com/cardavid/ms/item/api/controllers/ItemController.java
+++ ms-item-core/src/main/java/com/cardavid/ms/item/api/controllers/ItemController.java
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cardavid.ms.item.api.dto.ItemDto;
-import com.cardavid.ms.item.api.dto.ProductDto;
 import com.cardavid.ms.item.api.parameter.ProductParameter;
 import com.cardavid.ms.item.api.service.ItemService;
 
