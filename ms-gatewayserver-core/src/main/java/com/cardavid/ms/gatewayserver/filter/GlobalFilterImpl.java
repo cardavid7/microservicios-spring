26d8d9806a765a96e697fe21b5908486f0fc9c50
cardavid7 <cardavid7@gmail.com>
2026-03-24 17:54:40 -0500

    update gatewayserver with user properties

--- ms-gatewayserver-core/src/main/java/com/cardavid/ms/gatewayserver/filter/GlobalFilterImpl.java
+++ ms-gatewayserver-core/src/main/java/com/cardavid/ms/gatewayserver/filter/GlobalFilterImpl.java
@@ -17,33 +17,32 @@ import reactor.core.publisher.Mono;
 @Component
 public class GlobalFilterImpl implements GlobalFilter {
 
-	
 	private static final Logger logger = LoggerFactory.getLogger(GlobalFilterImpl.class);
 
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-		
+
 		logger.info(String.format("Ejecutando el filter antes del request..."));
 		exchange.getAttributes().put("token_audit", "abcdef");
 
 		Optional.ofNullable(exchange.getRequest().getPath()).ifPresent(request -> {
-			logger.info(String.format("RequestApi: %s",request.value()));
+			logger.info(String.format("RequestApi: %s", request.value()));
 
-			if (request.value().contains("item")){
+			if (request.value().contains("item")) {
 				String requestHeader = exchange.getRequest().getHeaders().getFirst("X-Request");
-				logger.info(String.format("requestHeader: "+requestHeader));
-				
+				logger.info(String.format("requestHeader: " + requestHeader));
+
 				Map<String, List<String>> map = exchange.getRequest().getQueryParams();
-				logger.info(String.format("RequestParameter: "+map.get("X-Parameter")));
+				logger.info(String.format("RequestParameter: " + map.get("X-Parameter")));
 			}
 		});
-		
-		return chain.filter(exchange).then(Mono.fromRunnable(() -> { 
+
+		return chain.filter(exchange).then(Mono.fromRunnable(() -> {
 			logger.info("Ejecutando el filter despues del request...");
-	        String token = exchange.getAttribute("token_audit");
-			
+			String token = exchange.getAttribute("token_audit");
+
 			exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "red").build());
-			//exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
+			// exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
 			exchange.getResponse().getHeaders().set("token", token);
 		}));
 	}
